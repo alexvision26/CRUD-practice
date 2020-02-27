02c2e1b4c251dd3dfa0ad5861c0b310b34e4040d
Alex Miller <alexmillerprojects@gmail.com>
2020-02-27 14:31:32 -0800

    set up structure, building utils

--- animals/src/components/Login.js
+++ animals/src/components/Login.js
@@ -7,6 +7,11 @@ export default function Login(props) {
         <div>
             <h1>Welcome to the Safari App!</h1>
             <h2>I can't show you more until you log in. Please build out a login.</h2>
+
+            <form>
+                <label>Username: <input type='text' name='username'/></label><br/>
+                <label>Password: <input type='password' name='password'/></label><br/>
+            </form>
         </div>
     )
 }
